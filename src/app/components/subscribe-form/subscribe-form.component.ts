47a17263aa4f44f8fe854cae35694c90221371ad
ariyeb <ariyeb@gmail.com>
2020-08-04 19:47:49 +0300

    form valditors

--- src/app/components/subscribe-form/subscribe-form.component.ts
+++ src/app/components/subscribe-form/subscribe-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Form, AbstractControl, Validators } from '@angular/forms';
+import { FormControl, FormGroup, AbstractControl, Validators, FormBuilder, ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-subscribe-form',
@@ -10,24 +10,124 @@ export class SubscribeFormComponent implements OnInit {
   // inputTitle = new FormControl('Initial value')
   subscribeForm: FormGroup;
   firstName: AbstractControl
-  constructor() { }
+  lastName
+  age
+  email
+  password
+  passwordRepeated
+  terms
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
-    this.subscribeForm = new FormGroup({
-      firstName: new FormControl('', [Validators.required]),
-      lastName: new FormControl(''),
-      age: new FormControl(0),
-      email: new FormControl(''),
-      password: new FormControl(''),
-      passwordRepeated: new FormControl(''),
-      terms: new FormControl(false)
+    // this.subscribeForm = new FormGroup({
+    //   firstName: new FormControl('', [Validators.required, Validators.minLength(4)]),
+    //   lastName: new FormControl('', [Validators.required]),
+    //   age: new FormControl(0, [Validators.required, Validators.min(12)]),
+    //   email: new FormControl('', [Validators.required]),
+    //   password: new FormControl('', [Validators.required]),
+    //   passwordRepeated: new FormControl('', [Validators.required]),
+    //   terms: new FormControl(false)
+    // })
+
+    this.subscribeForm = this.fb.group({
+      firstName: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        this.mosheValidator
+      ]],
+      lastName: ['', [Validators.required]],
+      age: [, [Validators.required, Validators.min(12)]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [
+        Validators.required,
+        this.passwordValidator
+      ]],
+      passwordRepeated: ['', [Validators.required]],
+      terms: [new FormControl(false), [Validators.requiredTrue]]
+    }, {
+      validators: this.passwordRepeatedValidator
     })
 
     this.firstName = this.subscribeForm.get('firstName')
+    this.lastName = this.subscribeForm.get('lastName')
+    this.age = this.subscribeForm.get('age')
+    this.email = this.subscribeForm.get('email')
+    this.password = this.subscribeForm.get('password')
+    this.passwordRepeated = this.subscribeForm.get('passwordRepeated')
+    this.terms = this.subscribeForm.get('terms')
   }
 
   // onClickButtonDefault() {
   //   this.inputTitle.setValue('Default')
   // }
+
+  invalidFirstNameMessage() {
+    const errors = this.firstName.errors
+    if (errors?.required) {
+      return 'You must enter your first name.'
+    }
+    if (errors?.minlength) {
+      return 'First name must be at least 4 characters.'
+    }
+    if (errors?.namemoshe) {
+      return 'First name cannot be MOSHE!!!'
+    }
+  }
+
+  invalidAgeMessage() {
+    const errors = this.age.errors
+    if (errors?.required) {
+      return 'You must enter your age.'
+    }
+
+    if (errors?.min) {
+      return 'You must be at least 12 years old to signup.'
+    }
+  }
+
+  invalidPsswordMessage() {
+    const errors = this.password.errors
+    if (errors?.required) {
+      return 'You must enter password'
+    }
+    if (errors?.passwordinvalid) {
+      return 'Passwors must incluse digits and cannot contain white spaces'
+    }
+  }
+
+  invalidPsswordRepeatedMessage() {
+    if (this.passwordRepeated.errors?.required) {
+      return 'You must repeat on the password'
+    }
+
+    if (this.subscribeForm.errors?.passwordnotrepeated) {
+      return 'Two passwords must ne identical.'
+    }
+  }
+
+  mosheValidator(control: AbstractControl): ValidationErrors | null {
+    const invalid = control.value.trim().toLowerCase() === 'moshe'
+    return invalid ? { 'namemoshe': control.value } : null
+  }
+
+  passwordValidator(control: AbstractControl): ValidationErrors | null {
+    const isIncludesWhiteSpace = control.value.includes(' ')
+    const isIncludesDigits = (/\d/).exec(control.value)
+    const invalid = !isIncludesDigits || isIncludesWhiteSpace
+    return invalid ? { 'passwordinvalid': true } : null
+  }
+
+  passwordRepeatedValidator(control: FormGroup): ValidationErrors | null {
+    const password = control.get('password').value
+    const passwordRepeated = control.get('passwordRepeated').value
+    return password !== passwordRepeated ? { 'passwordnotrepeated': true } : null
+  }
+
+  onSubmitSubscribeForm() {
+    console.log(this.subscribeForm.get('lastName').value)
+  }
+
+  // ng serve --liveReload=false
 
 }
